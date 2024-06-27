55c38c0eda34ed21ae1a3bbf14dc6936727fc67c
Samarth Gupta <102611901+Samarth622@users.noreply.github.com>
2024-06-27 18:45:07 +0530

    Add files via upload

---
+++ Subnet/outputs.tf
@@ -0,0 +1,4 @@
+output "subnet_id" {
+  description = "This is Subnet Id"
+  value = aws_subnet.example_subnet.id
+}
---
+++ Subnet/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id_value" {
+  description = "This is VPC id to create subnet"
+}
+
+variable "cidr_block_value" {
+  description = "This is CIDR value"
+}
