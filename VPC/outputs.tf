88a2dc2b72e836b21f8a063932ee4e2a43ac7b19
Samarth Gupta <102611901+Samarth622@users.noreply.github.com>
2024-06-27 18:41:56 +0530

    Add files via upload

---
+++ VPC/outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+  description = "Vpc Id"
+  value = aws_vpc.example_vpc.id
+}
---
+++ VPC/variables.tf
@@ -0,0 +1,7 @@
+variable "cidr_block_value" {
+  description = "This is cidr block value"
+}
+
+variable "instance_tenancy_value" {
+  description = "This is instance_tenancy value"
+}
